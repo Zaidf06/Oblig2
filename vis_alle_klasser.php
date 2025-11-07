b1502c6ca6607fccd2da648292e98af0a77c1a51
Zaid <102301613+Zaidf06@users.noreply.github.com>
2025-11-07 22:33:41 +0100

    Fix syntax errors and improve PHP code formatting

--- vis_alle_klasser.php
+++ vis_alle_klasser.php
@@ -1,13 +1,13 @@
-<?php  /* vis_alle_klasser */
+<?php  /* vis_alle_klasser /
 /
 /*  Programmet skriver ut alle registrerte klasser
-*/
-  include ("db.php");  /* tilkobling til database-serveren utf�rt og valg av database foretatt */
+/
+  include ("db.php");  / tilkobling til database-serveren utf�rt og valg av database foretatt /
 
   $sqlSetning="SELECT FROM klasse ORDER BY klassekode;";
   $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
-    /* SQL-setning sendt til database-serveren */
-    $antallRader=mysqli_num_rows($sqlResultat);  /* antall rader i resultatet beregnet */
+    /* SQL-setning sendt til database-serveren /
+    $antallRader=mysqli_num_rows($sqlResultat);  / antall rader i resultatet beregnet /
 
   print ("<h3>Registrerte klasser </h3>");
   print ("<table border=1>");
@@ -15,10 +15,10 @@
 
   for ($r=1;$r<=$antallRader;$r++)
     {
-      $rad=mysqli_fetch_array($sqlResultat);  /* ny rad hentet fra sp�rringsresultatet */
-      $klassekode=$rad["klassekode"];        /* ELLER $klassekode=$rad[0]; */
-      $klassenavn=$rad["klassenavn"];    /* ELLER $klassenavn=$rad[1]; */
-      $studiumkode=$rad["studiumkode"];    /* ELLER $studiumkode=$rad[2]; */
+      $rad=mysqli_fetch_array($sqlResultat);  / ny rad hentet fra sp�rringsresultatet /
+      $klassekode=$rad["klassekode"];        / ELLER $klassekode=$rad[0]; /
+      $klassenavn=$rad["klassenavn"];    / ELLER $klassenavn=$rad[1]; /
+      $studiumkode=$rad["studiumkode"];    / ELLER $studiumkode=$rad[2]; */
 
       print ("<tr> <td> $klassekode </td> <td> $klassenavn </td> <td> $studiumkode </td> </tr>");
 
