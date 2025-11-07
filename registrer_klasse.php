2e665ffa1a8b273c09ebf913e00ef8d1b161ce35
Zaid <102301613+Zaidf06@users.noreply.github.com>
2025-11-07 22:18:18 +0100

    Fix SQL query and clean up PHP code

--- registrer_klasse.php
+++ registrer_klasse.php
@@ -34,7 +34,7 @@ else
 {
 include("db.php"); /* tilkobling til database-serveren utført og valg av database foretatt */
 
-$sqlSetning="SELECT FROM klasse WHERE klassekode='$klassekode';";
+$sqlSetning="SELECT * FROM klasse WHERE klassekode='$klassekode';";
 $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
 $antallRader=mysqli_num_rows($sqlResultat); /* antall rader i resultatet beregnet */
 
@@ -54,5 +54,6 @@ print ("F&oslash;lgende klasse er n&aring; registrert: $klassekode $klassenavn $
 }
 
 ?>
+
 
 
